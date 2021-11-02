c0212ebfe97ad922c170b519101cf4413fdb0201
Thomas <thomas@craftacademy.se>
2021-11-02 11:06:01 +0100

    configures cypress

---
+++ specs/features/userCanVisitTheApplication.feature.js
@@ -0,0 +1,6 @@
+describe("User visiting the application url", () => {
+  it("is expected to see a header", () => {
+    cy.visit("/");
+    cy.get("h1").should("contain.text", "Address Book");
+  });
+});
