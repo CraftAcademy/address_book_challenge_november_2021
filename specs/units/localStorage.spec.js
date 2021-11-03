49e01ce9ea61068e90e734c7924400ce2e2c4a9d
Thomas <thomas@craftacademy.se>
2021-11-03 11:15:22 +0100

    end of localStorage testing

--- specs/units/localStorage.spec.js
+++ specs/units/localStorage.spec.js
@@ -8,26 +8,76 @@ describe("localStorage", () => {
   it(() => is.expected.to.have.property("data"));
 
   const entry_1 = {
-    name: 'Thomas',
-    phone: '12344567',
-    email: 'thomas@fakemail.com'
-  }
+    name: "Thomas",
+    phone: "12344567",
+    email: "thomas@fakemail.com",
+  };
 
-  describe.only('#setItem', () => {
-    let storedData
+  describe("#setItem", () => {
+    let storedData;
 
     before(() => {
-     $subject.setItem('entries', JSON.stringify(entry_1)) 
-     storedData = $subject.data
+      $subject.setItem("entries", JSON.stringify(entry_1));
+      storedData = $subject.data;
     });
 
-    it('is expected to store data in the chosen key', () => {
-      expect(storedData).to.have.own.property('entries')
+    it("is expected to store data in the chosen key", () => {
+      expect(storedData).to.have.own.property("entries");
     });
 
-    it('is expected to store the data as a string', () => {
-      expect(typeof storedData.entries).to.equal('string')
+    it("is expected to store the data as a string", () => {
+      expect(typeof storedData.entries).to.equal("string");
     });
-    
+  });
+
+  describe("#getItem", () => {
+    context("existing key", () => {
+      def("response", () => $subject.getItem("entries"));
+
+      before(() => {
+        $subject.setItem("entries", JSON.stringify(entry_1));
+      });
+
+      it("is expected to return a string", () => {
+        expect(typeof $response).to.equal("string");
+      });
+
+      it("is expected to contain the data", () => {
+        expect(JSON.parse($response)).to.eql(entry_1);
+      });
+    });
+
+    context("non existing key", () => {
+      def("response", () => $subject.getItem("nonExistingKey"));
+
+      it('is expected to reurn "undefined"', () => {
+        expect($response).to.equal(undefined);
+      });
+    });
+  });
+
+  describe("#removeItem", () => {
+    beforeEach(() => {
+      $subject.setItem("keyToRemove", "some value");
+    });
+    it("is expected to remove an item", () => {
+      $subject.removeItem("keyToRemove");
+      expect($subject.data).to.not.have.own.property("keyToRemove");
+    });
+  });
+
+  describe("#clear", () => {
+    beforeEach(() => {
+      $subject.setItem("myKey", "some value");
+      $subject.setItem("myKey2", "some value");
+      $subject.clear();
+    });
+    it("is expected to remove an item myKey", () => {
+      expect($subject.data).to.not.have.own.property("myKey");
+    });
+
+    it("is expected to remove an item myKey2", () => {
+      expect($subject.data).to.not.have.own.property("myKey2");
+    });
   });
 });
