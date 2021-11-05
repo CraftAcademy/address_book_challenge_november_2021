08dfb35ded36ea6ee7c429209c2547e127600e13
Thomas <thomas@craftacademy.se>
2021-11-05 15:55:15 +0100

    adds entries display functionality

--- specs/features/userCanVisitTheApplication.feature.js
+++ specs/features/userCanVisitTheApplication.feature.js
@@ -2,21 +2,25 @@ describe("User visiting the application url", () => {
   beforeEach(() => {
     cy.visit("/", {
       onBeforeLoad(window) {
-        // let addressBook = new AddressBook()
-        // addressBook.create({"name": "Thomas"})
-        window.localStorage.setItem('entries', '[{"name": "Thomas"}]')
+        window.localStorage.setItem('entries', '[{"name": "Thomas", "phone": "031-121212", "twitter": "@thomasochman"}, {"name": "Barack", "phone": "+1 202-121212", "twitter": "@barackobama"}]')
       }
     })
+    cy.get('[data-cy=address_list] ul').as('displayList')
+    cy.get('[data-cy=address_form]').as('addressForm')
   });
   it("is expected to see a header", () => {
     cy.get("h1").should("contain.text", "Address Book");
   });
 
   it('is expected to see a form', () => {
-    cy.get('[data-cy=address_form]').should('exist')//.and('be.displayed')
+    cy.get('@addressForm').should('exist')//.and('be.visible')
   });
 
   it('is expected to see an address list', () => {
-    cy.get('[data-cy=address_list]').should('exist')//.and('be.displayed')
+    cy.get('@displayList').should('exist').and('be.visible')
+  });
+
+  it('is expected to display 2 entries', () => {
+    cy.get('@displayList').children().should('have.length', 2)
   });
 });
