acc44b2aa730db8a9233ebf586973fe5b9bea572
Thomas <thomas@craftacademy.se>
2021-11-05 22:22:50 +0100

    adds form for creating entries

--- specs/features/userCanVisitTheApplication.feature.js
+++ specs/features/userCanVisitTheApplication.feature.js
@@ -5,8 +5,8 @@ describe("User visiting the application url", () => {
         window.localStorage.setItem('entries', '[{"name": "Thomas", "phone": "031-121212", "twitter": "@thomasochman"}, {"name": "Barack", "phone": "+1 202-121212", "twitter": "@barackobama"}]')
       }
     })
-    cy.get('[data-cy=address_list] ul').as('displayList')
-    cy.get('[data-cy=address_form]').as('addressForm')
+    cy.get('[name=address_list] ul').as('displayList')
+    cy.get('[name=address_form]').as('addressForm')
   });
   it("is expected to see a header", () => {
     cy.get("h1").should("contain.text", "Address Book");
@@ -14,6 +14,23 @@ describe("User visiting the application url", () => {
 
   it('is expected to see a form', () => {
     cy.get('@addressForm').should('exist')//.and('be.visible')
+  });
+
+  describe('submitting a form', () => {
+    beforeEach(() => {
+      cy.get('[name=name]').type('Anders')
+      cy.get('[name=phone]').type('0700-121212')
+      cy.get('[name=twitter]').type('Anders_Andersson')
+      cy.get('[name=submit]').click()
+    });
+    
+    it('is expected to display 2 entries', () => {
+      cy.get('@displayList').children().should('have.length', 3)
+    });
+
+    it('is expected to display the new entry', () => {
+      cy.get('@displayList').should('contain.text', 'Anders')
+    });
   });
 
   it('is expected to see an address list', () => {
